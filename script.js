91c6b4c69bc873b33e5a27ba497d2c5c52077122
vedad <vedolino2004@gmail.com>
2024-09-11 00:19:36 +0200

    refactor script.js

--- script.js
+++ script.js
@@ -1,137 +1,139 @@
-let myLibrary = [];
-let index = 0;
-
-let dialogEl = document.querySelector("#dialog");
-
-function Book(title, author, numberOfPages, read, cover = "") {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.read = read;
-    this.index = index++;
-    this.cover = cover;
+class Book {
+    static index = 0;
+    constructor(title, author, numberOfPages, read, cover = "") {
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.read = read;
+        this.cover = cover;
+        this.index = Book.index++;
+    }
 }
 
-let HarryPotter = new Book("Harry Potter and the Sorcerer's Stone", "J. K. Rowling", 223, false, "https://ia904703.us.archive.org/view_archive.php?archive=/9/items/l_covers_0012/l_covers_0012_02.zip&file=0012020813-L.jpg");
-let AtomicHabits = new Book("Atomic Habits", "James Clear", 320, true, "https://covers.openlibrary.org/b/id/14813724-M.jpg");
-let HitchhikerGuide = new Book("The Hitchhiker's Guide to the Galaxy", " Douglas Adams", 224, true, "https://ia600505.us.archive.org/view_archive.php?archive=/10/items/m_covers_0011/m_covers_0011_46.zip&file=0011464553-M.jpg");
+class Library {
+    constructor() {
+        this.myLibrary = [];
+        this.dialogEl = document.querySelector("#dialog");
+        this.bookshelf = document.querySelector(".bookshelf");
+        this.addSlot = document.querySelector(".book-slot-add");
+        this.addBookBtn = document.querySelector(".add-book");
+        this.dialogShow = document.querySelector(".show");
+        this.dialogClose = document.querySelector(".close");
+        this.dialogInputs = document.querySelector(".inputs");
+        this.init();
+    }
 
-function addBookToLib(book) {
-    myLibrary.push(book);
-}
+    init() {
+        this.bookshelf.innerHTML = "";
+        this.displayBooks();
+        this.createEventListners();
+        this.dialogBookAdd();
+    }
 
-addBookToLib(HarryPotter);
-addBookToLib(AtomicHabits);
-addBookToLib(HitchhikerGuide);
+    createEventListners() {
+        this.dialogShow.addEventListener("click", () => {
+            this.dialogEl.showModal();
+        });
 
-let displayBooks = () => {
-    let i = 0;
-    myLibrary.forEach(book => {
+        this.dialogClose.addEventListener("click", () => {
+            this.dialogEl.close();
+        });
+    }
 
-        /*book-slot creation*/
-        let slot = document.createElement("div");
+    addBooktoLib(book) {
+        this.myLibrary.push(book);
+    }
+
+    displayBooks() {
+        this.bookshelf.appendChild(this.addSlot);
+        let i = 0;
+        this.myLibrary.forEach(book => {
+            this.bookshelf.insertBefore(this.createBookSlot(book, i), this.addSlot);
+            i++;
+        })
+    }
+
+    createBookSlot(book, i) {
+        const slot = document.createElement("div");
         slot.classList.add("book-slot");
         slot.classList.add(i);
 
-        /*Details of the book*/
-        let details = document.createElement("div");
+        const details = document.createElement("div");
         details.classList.add("details");
-        let titleEl = document.createElement("h2");
-        let authorEl = document.createElement("h3");
-        let pagesEl = document.createElement("h3");
-        let readEl = document.createElement("h3");
-        titleEl.innerHTML = `${book.title}`;
+
+        const titleEl = document.createElement("h2");
+        const authorEl = document.createElement("h3");
+        const pagesEl = document.createElement("h3");
+        const readEl = document.createElement("h3");
+
+        titleEl.innerHTML = book.title;
         authorEl.innerHTML = `By ${book.author}`;
         pagesEl.innerHTML = `Pages: ${book.numberOfPages}`;
+
         readEl.innerHTML = `Have you read it:`;
         readEl.classList.add("read-h");
 
-        /*Checkbox functionality*/
-        let checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.checked = book.read;
-        checkbox.addEventListener("change", (event) => {
-            if (event.currentTarget.checked) {
-                book.read = true;
-            }
-            else {
-                book.read = false;
-            }
+        const checkboxEl = document.createElement("input");
+        checkboxEl.type = "checkbox";
+        checkboxEl.checked = book.read;
+        checkboxEl.addEventListener("change", (event) => {
+            book.read = event.currentTarget.checked;
         })
-        readEl.appendChild(checkbox);
 
+        readEl.appendChild(checkboxEl);
         details.appendChild(titleEl);
         details.appendChild(authorEl);
         details.appendChild(pagesEl);
         details.appendChild(readEl);
 
-        /*Image and remove button */
-        let rightSide = document.createElement("div");
+
+        const rightSide = document.createElement("div");
         rightSide.classList.add("right");
-        let delBtn = document.createElement("button");
+        const delBtn = document.createElement("button");
         delBtn.type = "button";
         delBtn.innerHTML = "Remove";
         delBtn.classList.add("delete-btn");
-        let bookCover = document.createElement("img");
+        delBtn.addEventListener("click", () => {
+            this.myLibrary = this.myLibrary.filter(bk => bk.index != slot.classList[1]);
+            slot.remove();
+        })
+
+        const bookCover = document.createElement("img");
         bookCover.src = book.cover;
         bookCover.setAttribute("onerror", "this.style.display='none'")
 
-        slot.appendChild(details);
         rightSide.appendChild(delBtn);
         rightSide.appendChild(bookCover);
+        slot.appendChild(details);
         slot.appendChild(rightSide);
-        document.querySelector(".bookshelf").insertBefore(slot, document.querySelector(".book-slot-add"));
-        i++;
-    })
-};
 
-let showDialog = () => {
-    document.querySelector(".show").addEventListener("click", () => {
-        dialogEl.showModal();
-    })
-}
+        return slot;
+    }
 
-let closeDialog = () => {
-    document.querySelector(".close").addEventListener("click", () => {
-        dialogEl.close();
-    })
-}
+    dialogBookAdd() {
+        this.addBookBtn.addEventListener("click", () => {
+            const formTitle = document.querySelector("#book-title").value;
+            const formAuthor = document.querySelector("#book-author").value;
+            const formPages = document.querySelector("#book-pages").value;
+            const formRead = document.querySelector("#book-status").checked;
+            const formCover = document.querySelector("#book-cover").value;
 
-let removeBook = () => {
-    document.querySelectorAll(".delete-btn").forEach(btn => {
-        let selectedSlot = btn.parentElement.parentElement;
-        btn.addEventListener("click", function () {
-            myLibrary = myLibrary.filter(book =>
-                book.index != selectedSlot.classList[1]);
-            selectedSlot.remove();
+            this.addBooktoLib(new Book(formTitle, formAuthor, formPages, formRead, formCover));
+            this.bookshelf.innerHTML = "";
+            this.displayBooks();
+            this.dialogEl.close();
+            this.dialogInputs.reset();
         })
-    });
+    }
 }
 
-let addBtn = document.querySelector(".add-book");
-addBtn.addEventListener("click", () => {
-    /*Form vales*/
-    let formTitle = document.querySelector("#book-title").value;
-    let formAuthor = document.querySelector("#book-author").value;
-    let formPages = document.querySelector("#book-pages").value;
-    let formRead = document.querySelector("#book-status").checked;
-    let formCover = document.querySelector("#book-cover").value;
-    document.querySelector(".bookshelf").innerHTML = " ";
-    document.querySelector(".bookshelf").innerHTML += `
-        <div class="book-slot-add">
-            <button type="button" class="show">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus-box</title><path d="M17,13H13V17H11V13H7V11H11V7H13V11H17M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z" /></svg>
-            </button>
-        </div>`;
-    addBookToLib(new Book(formTitle, formAuthor, formPages, formRead, formCover));
-    displayBooks();
-    showDialog();
-    removeBook();
-    dialogEl.close();
-    document.querySelector(".inputs").reset();
-})
+const library = new Library();
 
-displayBooks();
-removeBook();
-showDialog();
-closeDialog();
+let HarryPotter = new Book("Harry Potter and the Sorcerer's Stone", "J. K. Rowling", 223, false, "https://ia904703.us.archive.org/view_archive.php?archive=/9/items/l_covers_0012/l_covers_0012_02.zip&file=0012020813-L.jpg");
+let AtomicHabits = new Book("Atomic Habits", "James Clear", 320, true, "https://covers.openlibrary.org/b/id/14813724-M.jpg");
+let HitchhikerGuide = new Book("The Hitchhiker's Guide to the Galaxy", " Douglas Adams", 224, true, "https://ia600505.us.archive.org/view_archive.php?archive=/10/items/m_covers_0011/m_covers_0011_46.zip&file=0011464553-M.jpg");
+library.addBooktoLib(HarryPotter);
+library.addBooktoLib(AtomicHabits);
+library.addBooktoLib(HitchhikerGuide);
+
+library.displayBooks();
